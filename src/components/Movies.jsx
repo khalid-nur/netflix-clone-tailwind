97a7577a4609e762c156c70a2531cb564dc44a9b
khalid-nur <khalidnur150@gmail.com>
2023-07-22 21:25:00 -0500

    implement movie saving functionality for authenticated users

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,8 +1,33 @@
 import React, { useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../firebase";
+import { arrayUnion, updateDoc, doc } from "firebase/firestore";
 
 const Movies = ({ item }) => {
   const [like, setLike] = useState(false);
+  const [saved, setSaved] = useState(false);
+  const { user } = UserAuth();
+
+  console.log(user);
+
+  const movieID = doc(db, "users", `${user?.email}`);
+
+  const saveMovie = async () => {
+    if (user?.email) {
+      setLike(!like);
+      setSaved(true);
+      await updateDoc(movieID, {
+        savedMovies: arrayUnion({
+          id: item.id,
+          title: item.title,
+          img: item.backdrop_path,
+        }),
+      });
+    } else {
+      alert("please login to save a movie");
+    }
+  };
 
   return (
     <div className="w-[160px] inline-block cursor-pointer relative p-2 sm:w-[200px] md:w-[240px] lg:w-[280px]">
@@ -16,7 +41,7 @@ const Movies = ({ item }) => {
         <p className=" whitespace-normal text-xs font-bold flex justify-center items-center h-full text-center md:text-sm ">
           {item?.title}
         </p>
-        <p>
+        <p onClick={saveMovie}>
           {like ? (
             <FaHeart className="absolute top-4 left-4 text-gray-300" />
           ) : (
