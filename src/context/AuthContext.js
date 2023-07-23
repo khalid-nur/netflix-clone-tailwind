02087142525d5d9cbf9f053c2a0a7543b573230a
khalid-nur <khalidnur150@gmail.com>
2023-07-22 20:39:40 -0500

    implement user signup collection to store user information

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,18 +1,23 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore";
+
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
 
   const signUp = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
+    createUserWithEmailAndPassword(auth, email, password);
+    setDoc(doc(db, "users", email), {
+      savedMovies: [],
+    });
   };
 
   const logIn = (email, password) => {
